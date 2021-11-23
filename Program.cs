3eca6424c827cfef5d456e623ecba69b4bd615a8
Devaansh  Dudeja <devaanshdudeja@gmail.com>
2021-11-23 07:40:20 +0000

    This is the C# source code.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hello
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I love Visual Studio, .NET, the .NET Framework, Xamarin, and C#.");
+        }
+    }
+}
+
+
